b114520f2b6cb197b93b7ff8854cb41ad5c6fc34
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-05-03 23:28:20 +0100

    ft: add logic to parse the words out

--- src/Program.cs
+++ src/Program.cs
@@ -1,27 +1,23 @@
 ﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Reflection;
 
 namespace scrabbler;
 
 public class Program
 {
-    private const string SourceFileName = "./words.txt";
-    private static readonly string[] _words;
-
-    static Program() => _words = File.ReadLines(SourceFileName).ToArray();
+    private static string[] _words;
 
     public static Task<int> Main(string[] args)
     {
         var rootCommand = new RootCommand()
         {
-            new Option<string>(new[]
+            new Argument<string>("tiles", description: "Your tiles."),
+            new Option<string?>(new[]
             {
                 "--source-file", "-S"
-            }, () => SourceFileName, "A file with the words to be searched. Defaults to the in-built list."),
-            new Option<string>(new[]
-            {
-                "--tiles", "-T"
-            }, description: "Your tiles."),
-            new Option<string>(new[]
+            }, description: "A file with the words to be searched. Defaults to the in-built list."),
+            new Option<string?>(new[]
             {
                 "--constraints", "-C"
             }, description: "What target tiles you may want to incorporate."),
@@ -29,7 +25,32 @@ public class Program
 
         rootCommand.Description =
             "A console application to help generate possible scrabble words given a list of tile values and constraints";
-        
-        return  rootCommand.InvokeAsync(args);
+        rootCommand.SetHandler(
+            (string tiles, string? source, string? constraints) => { Process(tiles, source, constraints); });
+
+        return rootCommand.InvokeAsync(args);
+    }
+
+    private static void Process(string tiles, string? source, string? constraints)
+    {
+    }
+
+    private static IEnumerable<string> GetWords(string? source)
+    {
+        var def = Enumerable.Empty<string>();
+
+        if (source != null)
+            if (File.Exists(source))
+                return File.ReadAllLines(source);
+
+        var assembly = Assembly.GetExecutingAssembly();
+        using var stream = assembly.GetFile("words.txt");
+
+        if (stream == null)
+            return def;
+
+        using var sr = new StreamReader(stream);
+        var contents = sr.ReadToEnd();
+        return contents.Split("\n");
     }
 }
